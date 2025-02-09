bb1e37dd760a310152a89bdc8d8d79d988633dcf
brenda <115198624+brenda255@users.noreply.github.com>
2025-02-09 16:49:35 -0700

    modified main container, css, added js for sidebar btns

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const timeElement = document.getElementById("clock");
 
 //Digital Clock Function
-function updateClock(){
+function updateClock() {
     let now = new Date();
     let hours = now.getHours().toString().padStart(2, '0');
     let minutes = now.getMinutes().toString().padStart(2, '0');
@@ -10,7 +10,7 @@ function updateClock(){
     let amPm = hours >= 12 ? "PM" : "AM";
 
     hours = hours % 12 || 12;
-    
+
     timeElement.innerText = `${hours}:${minutes}:${seconds} ${amPm}`
 }
 
@@ -21,7 +21,7 @@ updateClock();
 //Background Color Changer
 let bgChanger = document.querySelector(".bg-color-changer");
 
-bgChanger.addEventListener('click', function(){
+bgChanger.addEventListener('click', function () {
     let red = Math.floor(Math.random() * 256);
     let green = Math.floor(Math.random() * 256);
     let blue = Math.floor(Math.random() * 256);
@@ -32,11 +32,75 @@ bgChanger.addEventListener('click', function(){
 
     let hexColor = `#${redHex}${greenHex}${blueHex}`;
     document.body.style.backgroundColor = hexColor;
-} )
+})
 
+//Menu Toggle Button
 const menuToggle = document.getElementById("menu-toggle");
 const sidebar = document.querySelector(".sidebar");
 
-menuToggle.addEventListener('click', function (){
+menuToggle.addEventListener('click', function () {
     sidebar.classList.toggle("active");
 })
+
+//Switch Between Sections
+
+let todoBtn = document.getElementById("to-do-list-btn");
+let counterBtn = document.getElementById("counter-btn");
+let tipCalcBtn = document.getElementById("tip-calculator-btn");
+let passGenBtn = document.getElementById("pass-generator-btn");
+let guessGameBtn = document.getElementById("guess-game-btn");
+
+let todoSection = document.getElementById("todo-list");
+let counterSection = document.getElementById("counter");
+let tipCalculatorSection = document.getElementById("tip-calculator");
+let passGenSection = document.getElementById("pass-generator");
+let guessGameSection = document.getElementById("guess-game");
+
+//hide all sections
+function hideAllSections() {
+    todoSection.classList.remove('active');
+    counterSection.classList.remove('active');
+    tipCalculatorSection.classList.remove('active');
+    passGenSection.classList.remove('active');
+    guessGameSection.classList.remove('active');
+}
+
+function setActiveButton(button) {
+    todoBtn.classList.remove('active');
+    counterBtn.classList.remove('active');
+    tipCalcBtn.classList.remove('active');
+    passGenBtn.classList.remove('active');
+    guessGameBtn.classList.remove('active');
+
+    button.classList.add('active');
+}
+
+todoBtn.addEventListener("click", function () {
+    hideAllSections();
+    todoSection.classList.add('active');
+    setActiveButton(todoBtn);
+});
+
+counterBtn.addEventListener("click", function () {
+    hideAllSections();
+    counterSection.classList.add('active');
+    setActiveButton(counterBtn);
+});
+
+tipCalcBtn.addEventListener("click", function () {
+    hideAllSections();
+    tipCalculatorSection.classList.add('active');
+    setActiveButton(tipCalcBtn);
+});
+
+passGenBtn.addEventListener("click", function () {
+    hideAllSections();
+    passGenSection.classList.add('active');
+    setActiveButton(passGenBtn);
+});
+
+guessGameBtn.addEventListener("click", function () {
+    hideAllSections();
+    guessGameSection.classList.add('active');
+    setActiveButton(guessGameBtn);
+});
